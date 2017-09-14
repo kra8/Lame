39aca88469a9cbe21dd3a5e3246e8b8c0984f3fd
K.Asai <kra.enmn0618.swift@softbank.ne.jp>
2017-09-14 18:07:28 +0900

    Update v1.0.4

--- src/Kernel.php
+++ src/Kernel.php
@@ -19,7 +19,7 @@ class Kernel
 
     public function __construct()
     {
-        $this->app = new Application('Laravel and Homestead set up.', 'v1.0.3');
+        $this->app = new Application('Laravel and Homestead set up.', 'v1.0.4');
         foreach ($this->commands as $commandClassName) {
             $command = new $commandClassName();
             $this->app->add($command);
